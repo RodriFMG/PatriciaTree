62865268518516b03f5efed6d80931de7a714c71
RODRIGO <rodrigo.meza@utec.edu.pe>
2024-06-10 11:58:35 -0500

    Push

--- Node.h
+++ Node.h
@@ -8,28 +8,87 @@
 #include <iostream>
 using namespace std;
 
+/* Declaración circular
+ * Lo enseñaron en progra 2 xd, permite tener una clase dentro de otra y viceversa, lo cual
+ * ambas podrán funcionar y declarar métodos de las mismas de manera consisntete.
+ * Esto lo hago para que cada nodo tenga una función hash independiente.
+ */
 
-struct Node{
+struct Node;
 
-    bool is_word{};
+class ArrayHash {
+private:
+    Node* node;
+
+public:
+    explicit ArrayHash(Node* node1);
+
+    Node* root();
+
+    Node* search(char enlace);
+
+    void insert(char enlace, Node* elm);
+
+    bool esta_o_no(char enlace);
+
+    void KillSelf();
+};
+
+struct Node {
+    bool is_word;
     string cadena;
-    bool leaf{};
+    bool leaf;
+    ArrayHash* hash;
     Node** children;
 
-    Node(){
-        is_word = false;
-        cadena = "";
+    Node();
 
-        /*Si leaf es true significa que es nodo hoja. */
-        leaf = true;
+    ~Node();
+};
 
-        children = new Node*[26];
-        for (int i = 0; i < 26; ++i) {
-            children[i] = nullptr;
-        }
+Node::Node() {
+    is_word = false;
+    cadena = "";
+    leaf = true;
+    children = new Node*[26];
+    for (int i = 0; i < 26; ++i) {
+        children[i] = nullptr;
     }
 
-};
+    hash = new ArrayHash(this);
+}
+
+Node::~Node() {
+    delete hash;
+    delete[] children;
+}
+
+ArrayHash::ArrayHash(Node* node1) : node(node1) {}
+
+Node* ArrayHash::root() {
+    return node;
+}
+
+Node* ArrayHash::search(char enlace) {
+    int ind = enlace - 'a';
+    return node->children[ind];
+}
+
+void ArrayHash::insert(char enlace, Node* elm) {
+    int ind = enlace - 'a';
+    node->children[ind] = elm;
+}
+
+bool ArrayHash::esta_o_no(char enlace) {
+    int ind = enlace - 'a';
+    return (node->children[ind] == nullptr);
+}
+
+void ArrayHash::KillSelf() {
+    for(int i = 0; i < 26; ++i) delete node->children[i];
+    delete[] node->children;
+    delete[] node;
+}
 
 
 #endif //PATRICIATREE_NODE_H
--- PatriciaTree.h
+++ PatriciaTree.h
@@ -5,19 +5,33 @@
 #ifndef PATRICIATREE_PATRICIATREE_H
 #define PATRICIATREE_PATRICIATREE_H
 
-#include "array.h"
+#include "Node.h"
 
 class PatriciaTree{
 private:
-    ArrayHash root;
+
+    Node* root;
+
 public:
-    PatriciaTree() = default;
+    PatriciaTree(){
+        root = new Node[26];
+    };
 
-    void insert(){
+    void insert(const string& word);
 
-    }
-
+    void RecursiveInsert(char prefijo, string word, Node* node);
 };
+
+
+void PatriciaTree::insert(const std::string &word) {
+
+}
+
+void PatriciaTree::RecursiveInsert(char prefijo, std::string word, Node* node) {
+
+
+}
+
 
 
 #endif //PATRICIATREE_PATRICIATREE_H

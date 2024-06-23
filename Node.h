823badfd0a6e37a5b93308f0b9e38fe164c71e16
RODRIGO <rodrigo.meza@utec.edu.pe>
2024-06-22 23:11:55 -0500

    Push

--- Node.h
+++ Node.h
@@ -24,27 +24,18 @@ private:
 public:
     explicit ArrayHash(Node* node1);
 
-    Node* root();
-
     Node* search(char enlace);
 
     void insert(char enlace, Node* elm);
 
     bool esta_o_no(char enlace);
 
-    void replace(char enlace);
 
     static int ind(char caracter);
 
     bool solo_un_hijo();
 
     char encontrar_unico_hijo();
-
-    bool encontrar_2_hijos();
-
-    bool encontrar_mas_de_2_hijos();
-
-    void KillSelf();
 };
 
 struct Node {
@@ -78,10 +69,6 @@ Node::~Node() {
 
 ArrayHash::ArrayHash(Node* node1) : node(node1) {}
 
-Node* ArrayHash::root() {
-    return node;
-}
-
 Node* ArrayHash::search(char enlace) {
     int ind = enlace - 'a';
     return node->children[ind];
@@ -98,19 +85,8 @@ bool ArrayHash::esta_o_no(char enlace) {
     return (node->children[ind] != nullptr);
 }
 
-void ArrayHash::KillSelf() {
-    for(int i = 0; i < 26; ++i) delete node->children[i];
-    delete[] node->children;
-    delete[] node;
-}
-
 int ArrayHash::ind(char caracter) {
     return caracter - 'a';
-}
-
-void ArrayHash::replace(char enlace) {
-    int ind = enlace - 'a';
-    node->children[ind] = nullptr;
 }
 
 bool ArrayHash::solo_un_hijo() {
@@ -142,26 +118,5 @@ char ArrayHash::encontrar_unico_hijo() {
         }
 }
 
-bool ArrayHash::encontrar_2_hijos() {
-    int ind{};
-    for(int i=0; i<26; ++i){
-        if(node->children[i]) ++ind;
-        if(ind==2) {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool ArrayHash::encontrar_mas_de_2_hijos() {
-    int ind{};
-    for(int i=0; i<26; ++i){
-        if(node->children[i]) ++ind;
-        if(ind>2) {
-            return true;
-        }
-    }
-    return false;
-}
 
 #endif //PATRICIATREE_NODE_H
